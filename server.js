919f49898de189ccf89e498ba8f480c19ee5af24
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-10-17 16:39:09 -0500

    added server updates

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const dummyRouter = require('./dummyRouter/dummyRouter.js')
 
 const server = express()
 
 server.use(express.json());
+server.use('/test', dummyRouter)
 
 server.get('/', (req, res) => {
     res.status(200).jason({message: "It\'s working"})
